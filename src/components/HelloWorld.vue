4262424bf685feef19189b175b6db7d7b6fb007c
Aditya Doshi <adityadoshi25@gmail.com>
2026-07-09 00:35:52 +0530

    Initial commit - vue scaffolding

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Hello World</h1>
+  <div class="flex items-center justify-center bg-blue-100">
+    <h1 class="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+  </div>
+</template>
